4042a2f7ab493c1e587bc30421ef6b5e6251b76f
Parmod Kumar <parmod25feb@gmail.com>
2024-04-10 15:33:54 -0500

    level1 star pattern

---
+++ starpattern0410.py
@@ -0,0 +1,18 @@
+# This program will show the star pattern
+# Date : 10th April, 2024
+# Owner : Parmod Kumar
+
+for i in range(1,6):
+    for j in range(0,i):
+        print("*", end='')
+    print('')
+
+
+# Output : 
+
+# *
+# **
+# ***
+# ****
+# *****
+
