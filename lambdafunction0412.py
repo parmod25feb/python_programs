2ff2808ed0424b31818cac3514be75ce65c6b2cb
Parmod Kumar <parmod25feb@gmail.com>
2024-04-12 17:57:26 -0500

    lambda function

---
+++ lambdafunction0412.py
@@ -0,0 +1,15 @@
+# This program will explain the concept of lambda function
+# Date : 12th April, 2024
+# Owner : Parmod Kumar
+
+a,b = 7,8
+
+# Sum with lambda function
+sum = lambda a,b : a+b
+s=sum(a,b)
+print(f"Sum of {a,b} is : {s}")
+
+# Multiplication with lambda function
+mul = lambda a,b : a*b
+m=mul(a,b)
+print(f"MUltiplication for {a,b} is: {m}")
