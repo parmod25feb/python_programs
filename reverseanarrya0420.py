b699e22328b806e18c8703bd2858eca115eb47c6
Parmod Kumar <parmod25feb@gmail.com>
2024-04-20 21:06:32 -0500

    reverse of an array on an interval of 3

---
+++ reverseanarrya0420.py
@@ -0,0 +1,7 @@
+# The program is to reverse an array - Reverse an array of 9 elements on an interval of 3 - [1,2,3,4,5,6,7,8,9] = [3,2,1,6,5,4,9,8,7] 
+# Date : 20th April, 2024
+# Author : Parmod Kumar
+
+ar =  [1,2,3,4,5,6,7,8,9]
+
+print(ar)
