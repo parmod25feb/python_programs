4df23f463a7a355beb78121eb3f8e54ef75289c7
Parmod Kumar <parmod25feb@gmail.com>
2024-04-20 23:25:13 -0500

    Reverser array on an interval progam is now completed

--- reverseanarrya0420.py
+++ reverseanarrya0420.py
@@ -2,6 +2,25 @@
 # Date : 20th April, 2024
 # Author : Parmod Kumar
 
-ar =  [1,2,3,4,5,6,7,8,9]
+ar =  [1,2,3,4,5,6,7,8,9]\
 
-print(ar)
+# print the original array
+print(f"\nOriginal array is : {ar}")
+
+# Definied the function to reverse the array in the interval of 3 and then print the output
+def arr_reverse(ar):
+    ls=[]
+    start= 0 
+    end = 3
+    for i in range(1,4):
+        for l in range(end-1,start-1,-1):
+            ls.append(ar[l])
+        start +=3
+        end +=3
+
+    print(f"\nFinal arrya is : {ls}\n")
+
+# Calling the function to get the reverse
+arr_reverse(ar)
+
+
